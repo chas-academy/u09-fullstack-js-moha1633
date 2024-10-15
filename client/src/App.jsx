05af96331fb672518146e48ba8c7888d3ed7aaea
mohamed abdi <mgudaal6@gmail.com>
2024-10-15 18:01:42 +0200

    skåpade på footer användade flowbite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,19 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import { Footer } from 'flowbite-react'
+import MyFooter from './components/MyFooter'
 
 function App() {
   return (
     <>
     <Navbar/>
+    <div className='min-h-screen'>
+
     <Outlet/>
+
+    </div>
+    <MyFooter/>
     </>
   )
 }
