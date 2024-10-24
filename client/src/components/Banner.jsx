c604d3c84f35c735d2168ff9b4e5550024c79a57
mohamed abdi <mgudaal6@gmail.com>
2024-10-24 02:24:18 +0200

    change color

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -3,7 +3,7 @@ import { BannerCard } from '../home/BannerCard'
 
 const Banner = () => {
   return (
-    <div className='px-4 lg:px-24 bg-teal-100 flex items-center'>
+    <div className='px-4 lg:px-24 bg-blend-teal flex items-center'>
       <div className='flex w-full flex-col md:flex-row justify-between items-center gap-12 py-40'>
 
 
@@ -12,7 +12,7 @@ const Banner = () => {
         <div className='md:w-1/2 space-x-8 h-full'>
 
         
-        <h2 className='text-5xl font-bold leading-snug text-black'>Buy and sell Your Books <span className='text-blue-700'>for the Best prices</span></h2>
+        <h2 className='text-5xl font-bold leading-snug text-black'>Buy and sell Your Books <span className='text-red-700'>for the Best prices</span></h2>
         <p className='md:w-4/5 '>
           Hitta de bästa böckerna till fantastiska priser eller sälj dina
           använda böcker till andra bokälskare. Oavsett om du letar efter en
@@ -22,7 +22,7 @@ const Banner = () => {
         </p>
         <div className='flex space-x-2'>
             <input type="search" name='search' id='search' placeholder='search abook' className='py-2 px-2 rounded-s-sm outline-none' />
-            <button className='bg-blue-700 px-6 py-2 text-white font-medium hover:bg-black transition-all ease-in duration-200'>Search</button>
+            <button className='bg-red-700 px-6 py-2 text-white font-medium hover:bg-black transition-all ease-in duration-200'>Search</button>
         </div>
         </div>
         {/* right side  */}
